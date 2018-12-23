345f3ad3273be8de93f24053d43695989569ab97
Mike Liao <mikeliao@hsin-mins-mac-mini.local>
2018-12-23 22:23:21 +0800

    Initial Commit

---
+++ concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by Hsin-Min Liao on 2018/12/23.
+//  Copyright © 2018 Hsin-Min Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
