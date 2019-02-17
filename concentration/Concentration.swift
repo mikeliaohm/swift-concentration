3462ad342f2b91cab1b85d0cef2949a5f8bef7fd
Mike Liao <mike.hm.liao@gmail.com>
2019-02-17 13:25:12 +0800

    utilize protocol to enforce oneAndOnly that returns the first element or nil if there are multiple elements in the Collection

--- concentration/Concentration.swift
+++ concentration/Concentration.swift
@@ -16,8 +16,7 @@ struct Concentration
     // computed var with get and set (mutable)
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            let faceUpCardIndices = cards.indices.filter { cards[$0].isFaceUp }
-            return faceUpCardIndices.count == 1 ? faceUpCardIndices.first : nil
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
 //            var foundIndex: Int?
 //            for index in cards.indices {
 //                if cards[index].isFaceUp {
