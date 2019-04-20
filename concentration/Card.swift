eb90a670b13e7c8efa186d3d3684fb8014e320bf
Mike Liao <mike.hm.liao@gmail.com>
2019-04-20 16:39:45 +0800

    implement has(into: ) method to conform to Hashable

--- concentration/Card.swift
+++ concentration/Card.swift
@@ -10,7 +10,10 @@ import Foundation
 
 struct Card: Hashable
 {
-    var hashValue: Int { return identifier }
+//    var hashValue: Int { return identifier }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
     
     static func ==(lhs: Card, rhs: Card) -> Bool {
         return lhs.identifier == rhs.identifier
